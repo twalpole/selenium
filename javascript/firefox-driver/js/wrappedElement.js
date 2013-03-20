fc88f32a9f314e46c667a1d9c5067c6593fc7a5f
Thomas Walpole <twalpole@gmail.com>
2013-03-19 18:02:28 -0700

    multiple file upload support for firefox

--- javascript/firefox-driver/js/wrappedElement.js
+++ javascript/firefox-driver/js/wrappedElement.js
@@ -190,7 +190,6 @@ WebElement.sendKeysToElement = function(respond, parameters) {
   var alreadyFocused = true;
   var currentlyActive = Utils.getActiveElement(respond.session.getDocument());
   var newDocument = goog.dom.getOwnerDocument(currentlyActive);
-
   if (currentlyActive != element || currentDocument != new XPCNativeWrapper(newDocument)) {
     goog.log.info(WebElement.LOG_, 'Need to switch focus');
     alreadyFocused = false;
@@ -216,7 +215,11 @@ WebElement.sendKeysToElement = function(respond, parameters) {
   if (bot.dom.isElement(element, goog.dom.TagName.INPUT)) {
     var inputtype = element.getAttribute('type');
     if (inputtype && inputtype.toLowerCase() == 'file') {
-      element.value = parameters.value.join('');
+      if (element.getAttribute('multiple')) {
+        element.mozSetFileNameArray(parameters.value, parameters.value.length)
+      } else {
+        element.value = parameters.value.join('');
+      }
       Utils.fireHtmlEvent(element, 'change');
       respond.send();
       return;
